12ff08563a95463d4f28e7d148beb809994ac3d3
nikita1984 <52231270+nikita1984@users.noreply.github.com>
2021-10-18 20:55:50 +0300

    add titel and descripton elements for my future cart

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-const a = 5;
-document.write(a);
+const elem = document.getElementById("root");
+
+const mapTitel = document.createElement("h1");
+mapTitel.className = "titel";
+mapTitel.textContent = "My test map";
+
+const mapDescription = document.createElement("p");
+mapDescription.className = "description";
+mapDescription.textContent = "Ниже будет расположена моя карта, созданная с помощью библиотеки Leaflet";
+
+elem.appendChild(mapTitel);
+elem.appendChild(mapDescription);
