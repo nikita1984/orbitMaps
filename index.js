7b3463a709bcd66f761df0ddab550ff7c5ef39bb
nikita1984 <52231270+nikita1984@users.noreply.github.com>
2021-10-19 10:57:38 +0300

    add map from Leaflet (2)

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const elem = document.getElementById("root");
-
 const mapTitel = document.createElement("h1");
 mapTitel.className = "titel";
 mapTitel.textContent = "My test map";
@@ -8,5 +6,12 @@ const mapDescription = document.createElement("p");
 mapDescription.classList.add('description');
 mapDescription.textContent = "Ниже будет расположена моя карта, созданная с помощью библиотеки Leaflet";
 
+const elem = document.getElementById("root");
 elem.appendChild(mapTitel);
 elem.appendChild(mapDescription);
+
+const appMap = document.getElementById("mapid");
+appMap.style.height = '180px';
+appMap.style.width = '500px';
+
+const mymap = L.map('mapid').setView([51.505, -0.09], 13);
