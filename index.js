ecaa8266a4b347b0954fd54100d5e7097b87240d
nikita1984 <52231270+nikita1984@users.noreply.github.com>
2021-10-19 18:13:46 +0300

    add Polygon

--- index.js
+++ index.js
@@ -33,3 +33,9 @@ const circle = L.circle([51.508, -0.11], {
     fillOpacity: 0.5,
     radius: 500
 }).addTo(mymap);
+
+const polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(mymap);
