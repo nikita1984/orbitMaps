855bb0197e86c942d6f21a8bf27db90f9e3e158b
nikita1984 <52231270+nikita1984@users.noreply.github.com>
2021-10-19 18:31:19 +0300

    add comment for code

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
+// создаём <h1 class="titel">My test map<h1>
 const mapTitel = document.createElement("h1");
 mapTitel.className = "titel";
 mapTitel.textContent = "My test map";
 
+// создаём <p class="description">Ниже будет расположена моя карта, созданная с помощью библиотеки Leaflet<p>
 const mapDescription = document.createElement("p");
 mapDescription.classList.add('description');
 mapDescription.textContent = "Ниже будет расположена моя карта, созданная с помощью библиотеки Leaflet";
 
+// внедряем созданные h1 & p в html
 const elem = document.getElementById("root");
 elem.appendChild(mapTitel);
 elem.appendChild(mapDescription);
 
+// cоздаём окно в html для подсоединения карты Leaflet
 const appMap = document.getElementById("mapid");
 appMap.style.height = '400px';
 appMap.style.width = '600px';
 
+/* с помощью метода map() создаём путём интеграции карту с центром по координатам (51.505, -0.09) и значением
+    уровня масштабирования 13 */
 const mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
+// добавляем слой  к нашей карте
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -25,8 +32,10 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1Ijoic3RuZXViYXVlciIsImEiOiJja3V4c3dtY2YwamhyMnBxd293bWFxa3VwIn0.U4Tf59nxNybfo1wUANFrkA'
 }).addTo(mymap);
 
+// добавляем маркер навигации к карте
 const marker = L.marker([51.5, -0.09]).addTo(mymap);
 
+// добавляем круг к карте
 const circle = L.circle([51.508, -0.11], {
     color: 'red',
     fillColor: '#f03',
@@ -34,8 +43,15 @@ const circle = L.circle([51.508, -0.11], {
     radius: 500
 }).addTo(mymap);
 
+// отмечаем метку в форме треугольника на карте
 const polygon = L.polygon([
     [51.509, -0.08],
     [51.503, -0.06],
     [51.51, -0.047]
 ]).addTo(mymap);
+
+/* добавляем всплыв.окна которые будут высвечиваться при hover на метках карты
+при первоначальной загрузке будет отображаться popup на метке marker  */
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
